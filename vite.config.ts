dff15c8d07a0f42d73e5377ee438350a7168fee3
VisualBytess <info.visualbytes@gmail.com>
2025-04-17 13:56:12 +0200

    update
    updatee

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Singularity/', // 👈 QUESTA È LA MAGIA
+
   server: {
     host: "::",
     port: 8080,
